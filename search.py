1d9e676ac6018eacc421ddfc94dfcf0c3e20cb35
csokapeter <55291376+csokapeter@users.noreply.github.com>
2021-10-03 18:20:38 +0200

    Update search.py

--- search.py
+++ search.py
@@ -2,25 +2,30 @@ import requests
 from bs4 import BeautifulSoup
 from googlesearch import search
 
-def search(title):
+
+def find_ratings(title):
     query = title + " site=\"imdb.com\""
-    url = next(search(query, stop=1), None)
+    urls = search(query)
+    for x in urls:
+        url = x
+        break
     headers = {"Accept-Language": "en-US, en;q=0.5"}
     result = requests.get(url, headers=headers)
     site = BeautifulSoup(result.text, "html.parser")
     number_of_seasons = site.find('label', class_='ipc-simple-select__label').text.split()[0]
+    print(number_of_seasons)
 
     season_urls = []
     for i in range(1,int(number_of_seasons)+1):
         season_urls.append(url + "episodes?season=" + str(i))
 
     ratings = []
-    for url in season_urls:
-        result = requests.get(url)
+    for urls in season_urls:
+        result = requests.get(urls)
         site = BeautifulSoup(result.text, "html.parser")
         rating_divs = site.find_all('div', class_='ipl-rating-star small')
         season_rating = []
         for div in rating_divs:
             season_rating.append(float(div.find('span', class_='ipl-rating-star__rating').text))
         ratings.append(season_rating)
-    return(ratings)
+    print(ratings)
